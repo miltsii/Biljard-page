288912192ad58615ce225e0c58216eb98506ee60
miltsii <millaemiliat@gmail.com>
2026-09-04 21:55:59 +0300

    Imported all the necessary things on app.py and finished register.html

---
+++ app.py
@@ -0,0 +1,10 @@
+import secrets
+import sqlite3
+
+from flask import Flask, abort, flash, g, redirect, render_template, request, session, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.secret_key = "kehitys-avain-vaihda-tuotannossa"  
+
+DATABASE = "database.db"
